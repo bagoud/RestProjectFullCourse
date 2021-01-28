b249e0de2da34894b8cb320f52d6fa6a24e8ef3d
saikrishnabaddam <krrishna.ashmith@gmail.com>
2021-01-28 18:19:03 +0530

    second commit

---
+++ src/main/java/com/learning/CompleteRestCourse/CompleteRestCourseApplication.java
@@ -0,0 +1,13 @@
+package com.learning.CompleteRestCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteRestCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteRestCourseApplication.class, args);
+	}
+
+}
